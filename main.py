830ea469c7fa69e33c4cdac9f397d890ba694fe9
Zolotin <lvenokkuznetsoff@yandex.ru>
2024-08-10 19:27:32 +0300

    Add file

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import telebot
+from telebot import types
 token=('7431473620:AAGhcm3uWIrSlQfBX-OovOvBYE4W2dx23Jw')
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Привет ✌️ ")
+    bot.send_message(message.chat.id,'Привет')
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        bot.send_message(message.chat.id,"https://habr.com/ru/users/lubaznatel/")
 bot.infinity_polling()
